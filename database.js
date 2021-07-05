11dc4e1c2903b228a051fb573a546342bddb61cc
menezesmario <mario082@gmail.com>
2021-07-05 14:20:32 -0300

    database created

---
+++ database.js
@@ -0,0 +1,47 @@
+const books = [
+    {
+        id: 1,
+        name: "Digital Minimalism",
+        author: "Cal Newport",
+        category: "Produtividade e estilo de vida",
+        pages: 254,
+        recommends: false,
+        read: false
+    },
+    {
+        id: 2,
+        name: "O Genocídio do Negro Brasileiro",
+        author: "Abdias do Nascimento",
+        category: "História Brasileira",
+        pages: 254,
+        recommends: false,
+        read: false
+    },
+    {
+        id: 3,
+        name: "As Veias Abertas da América Latina",
+        author: "Eduardo Galeano",
+        category: "América",
+        pages: 400,
+        recommends: false,
+        read: false
+    },
+    {
+        id: 4,
+        name: "Algoritmos para viver",
+        author: "Brian Christian",
+        category: "Tecnologia",
+        pages: 412,
+        recommends: true,
+        read: true
+    },
+    {
+        id: 5,
+        name: "Thinking, fast and slow",
+        author: "Daniel Kahneman",
+        category: "Estilo de vida",
+        pages: 418,
+        recommends: true,
+        read: false
+    }
+]
